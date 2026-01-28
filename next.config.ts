504544459581119960637a3f737535766f92db80
Joao-Adegas <pedrojoao6378@gmail.com>
2026-01-27 23:39:51 -0300

    Add Sidebar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import test from "node:test";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack:{
+    rules:{
+      "*.svg":{
+        loaders:["@svgr/webpack"],
+        as: "*.js"
+      }
+    }
+  },
+  webpack (config) {
+    config.module.rules.push({
+      test: /\.svg$/
+    })
+  }
 };
 
 export default nextConfig;
---
+++ src/components/shared/app-sidebar/index.tsx
@@ -0,0 +1,22 @@
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail } from "@/components/ui/sidebar";
+import { ComponentProps } from "react";
+import Link from "next/link";
+
+type AppSidebarProps = ComponentProps<typeof Sidebar>
+
+export const AppSidebar = ({...props}: AppSidebarProps) => {
+    return(
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader className="py-4">
+                <Link href="/"><p>Logo</p> </Link>
+            </SidebarHeader>
+            <SidebarContent>
+                <p>Sidebar content</p>
+            </SidebarContent>
+            <SidebarFooter>
+                <p>Sidebar footer</p>
+            </SidebarFooter>
+            <SidebarRail/> 
+        </Sidebar>
+    )
+}
